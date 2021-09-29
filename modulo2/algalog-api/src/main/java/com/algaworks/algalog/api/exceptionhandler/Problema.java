dcfe2592a65f8e5fbc63962700f22a662977d267
Ricardo Oliveira <57198074+RicardoOliveira95@users.noreply.github.com>
2021-09-29 02:34:15 +0100

    Add files via upload

---
+++ modulo2/algalog-api/src/main/java/com/algaworks/algalog/api/exceptionhandler/Problema.java
@@ -0,0 +1,68 @@
+package com.algaworks.algalog.api.exceptionhandler;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@JsonInclude(Include.NON_NULL)
+@Getter
+@Setter
+public class Problema {
+
+	private Integer status;
+	private LocalDateTime dataHora;
+	private String titulo;
+	private List<Campo> campos;
+	
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public LocalDateTime getDataHora() {
+		return dataHora;
+	}
+
+	public void setDataHora(LocalDateTime dataHora) {
+		this.dataHora = dataHora;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public List<Campo> getCampos() {
+		return campos;
+	}
+
+	public void setCampos(List<Campo> campos) {
+		this.campos = campos;
+	}
+
+	@AllArgsConstructor
+	@Getter
+	public static class Campo {
+		
+		public Campo(String nome, String mensagem) {
+			this.nome=nome;
+			this.mensagem=mensagem;
+		}
+		private String nome;
+		private String mensagem;
+		
+	}
+	
+}
